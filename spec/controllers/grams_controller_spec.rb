6889f2c8d230848330f0f96320c463c1f9282088
sebtn <tonomudo@gmail.com>
2016-01-24 23:18:52 +0000

    Tdd for models user and gram conection, user verification

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -10,25 +10,54 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#new action" do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end  
   	it "should succesfully show the new form" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+        )
+      sign_in user
+
   		get :new
   		expect(response).to have_http_status(:success)
   	end
   end	
 
   describe "grams#create action" do
+    it "should require users to be logged in" do 
+      post :create, gram: { message: "Hello!" }
+    end  
     it "should create a new gram in our database" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+        )
+      sign_in user
+
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
       
       gram = Gram.last                 
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
       #which should be the gram that was just created. 
       #The second line -> hello! = hello! verifies the message is what we just posted.
       # The first line -> Gram.last that was added loads the last gram in our database, 
     end  
 
     it "should properly deal with validation errors" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+        )
+      sign_in user
+
       post :create, gram: {message: '' }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
