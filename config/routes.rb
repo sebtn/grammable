1f7f7ef179b9980bf9fa4b269c65f62470f5b24b
sebtn <tonomudo@gmail.com>
2016-01-25 16:10:08 +0000

    Tdd for destroying grams

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Grammable::Application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams, only: [:new, :create, :show, :edit, :update]
+  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+  #resources :grams -> this will indicate that we have all crud actions connected to a
+  # controller, we cuold just use this one for DRY code
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#destroy" do
+    it "should allow a user to destroy grams" do
+      g = FactoryGirl.create(:gram)
+      delete :destroy, id: g.id
+      expect(response).to redirect_to root_path
+      g = Gram.find_by_id(g.id)
+      expect(g).to eq nil
+    
+    end
+
+    it "should retunr 404 message if a gram with that specific id is not found" do
+      delete :destroy, id: 'SPACEDUCK'
+      expect(response).to have_http_status(:not_found)
+    end
+  end   
+
   describe "grams#update" do
     it "should allow users succesfully update grams" do
       p = FactoryGirl.create(:gram, message: "Initial Value")
