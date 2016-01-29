32b69f04dafc0646a660ee81c9a79ffd77660740
sebtn <tonomudo@gmail.com>
2016-01-29 22:00:13 +0000

    Tdd for comments capability

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
-FactoryGirl.define do
+FactoryGirl.define do  factory :comment, :class => 'Comments' do
+    
+  end
+
   factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
