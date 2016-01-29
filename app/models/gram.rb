03c2e29bc8102e166cbc2458f11b8ec6d757c4ee
sebtn <tonomudo@gmail.com>
2016-01-29 20:56:56 +0000

    Tested image uploading Rsepc and factories

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,8 +1,9 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true
-  belongs_to :user
+  validates :picture, presence: true
 
   mount_uploader :picture, PictureUploader
 
+  belongs_to :user
 
 end
